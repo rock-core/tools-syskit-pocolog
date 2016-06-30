95d919cc4ae68ae6e5b22203693b87bd2869120d
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2016-06-30 11:18:58 -0300

    cli: unify 'datastore auto-import' as 'datastore import --auto'

--- lib/syskit/pocolog/cli/datastore.rb
+++ lib/syskit/pocolog/cli/datastore.rb
@@ -45,52 +45,51 @@ module Syskit::Pocolog
             end
 
             desc 'import DATASTORE_PATH PATH', 'normalize and import a raw dataset into a syskit-pocolog datastore'
+            method_option :auto, desc: 'import all datasets under PATH',
+                type: :boolean, default: false
             method_option :silent, desc: 'do not display progress',
                 type: :boolean, default: false
             method_option :force, desc: 'overwrite existing datasets',
                 type: :boolean, default: false
-            def import(datastore_path, dataset_path)
-                datastore_path = Pathname.new(datastore_path)
-                datastore = Syskit::Pocolog::Datastore.create(datastore_path)
-                dataset_path   = Pathname.new(dataset_path).realpath
-                Syskit::Pocolog::Datastore.import(datastore, dataset_path, force: options[:force], silent: options[:silent])
-            end
-
-            desc 'auto-import DATASTORE_PATH PATH', 'import all folders looking like Syskit datasets under PATH into the datastore at DATASTORE_PATH'
-            method_option :silent, desc: 'do not display progress',
-                type: :boolean, default: false
-            method_option :force, desc: 'overwrite existing datasets',
-                type: :boolean, default: false
-            method_option :min_duration, desc: 'ignore datasets that last for less than this many seconds. Defaults to one minutes, set to zero to disable',
+            method_option :min_duration, desc: 'skip datasets whose duration is lower than this (in seconds)',
                 type: :numeric, default: 60
-            def auto_import(datastore_path, root_path)
+            def import(datastore_path, root_path)
                 root_path = Pathname.new(root_path).realpath
-                datastore_path = Pathname.new(datastore_path)
-                datastore_path.mkpath
-                store = Syskit::Pocolog::Datastore.create(datastore_path)
+                if options[:auto]
+                    paths = Array.new
+                    root_path.find do |p|
+                        is_raw_dataset = p.directory? &&
+                            Pathname.enum_for(:glob, p + "*-events.log").any? { true } &&
+                            Pathname.enum_for(:glob, p + "*.0.log").any? { true }
+                        if is_raw_dataset
+                            paths << p
+                            Find.prune
+                        end
+                    end
+                else
+                    paths = [root_path]
+                end
 
                 pastel = Pastel.new
 
-                root_path.find do |p|
-                    next if !p.directory?
-                    next if !Pathname.enum_for(:glob, p + "*-events.log").any? { true }
-                    next if !Pathname.enum_for(:glob, p + "*.0.log").any? { true }
-
+                datastore_path = Pathname.new(datastore_path)
+                datastore = Syskit::Pocolog::Datastore.create(datastore_path)
+                paths.each do |p|
                     if !options[:silent]
                         $stderr.puts pastel.bold("Processing #{p}")
                     end
 
                     last_import_digest, last_import_time = Syskit::Pocolog::Datastore::Import.find_import_info(p)
-                    already_imported = (last_import_digest && store.has?(last_import_digest))
+                    already_imported = (last_import_digest && datastore.has?(last_import_digest))
                     if already_imported && !options[:force]
                         if !options[:silent]
                             $stderr.puts pastel.yellow("#{p} already seem to have been imported as #{last_import_digest} at #{last_import_time}. Give --force to import again")
+                            next
                         end
-                        Find.prune
                     end
 
-                    store.in_incoming do |core_path, cache_path|
-                        importer = Syskit::Pocolog::Datastore::Import.new(store)
+                    datastore.in_incoming do |core_path, cache_path|
+                        importer = Syskit::Pocolog::Datastore::Import.new(datastore)
                         dataset = importer.normalize_dataset(p, core_path, cache_path: cache_path, silent: options[:silent])
                         stream_duration = dataset.each_pocolog_stream.map do |stream|
                             stream.duration_lg
@@ -101,7 +100,7 @@ module Syskit::Pocolog
                             # --force is implied as otherwise we would have
                             # skipped earlier
                             $stderr.puts pastel.yellow("#{p} seem to have already been imported but --force is given, overwriting")
-                            store.delete(last_import_digest)
+                            datastore.delete(last_import_digest)
                         end
 
                         if stream_duration >= options[:min_duration]
@@ -114,8 +113,6 @@ module Syskit::Pocolog
                             $stderr.puts pastel.yellow("#{p} lasts only %.1fs, ignored" % [stream_duration])
                         end
                     end
-
-                    Find.prune
                 end
             end
         end
--- test/cli/datastore_test.rb
+++ test/cli/datastore_test.rb
@@ -29,15 +29,8 @@ module Syskit::Pocolog
                 Datastore.start([*args, *extra_args], debug: true)
             end
 
-            describe "#auto_import" do
-                it "creates the datastore path" do
-                    datastore_path.rmtree
-                    call_cli('auto-import', datastore_path.to_s, root_path.to_s)
-                    assert datastore_path.exist?
-                end
-                it "auto-imports any directory that looks like a raw dataset" do
-                    create_logfile('test.0.log') {}
-                    FileUtils.touch logfile_path('test-events.log')
+            describe "#import" do
+                it "imports a single dataset into the store" do
                     incoming_path = datastore_path + 'incoming' + '0'
                     flexmock(datastore_m::Import).new_instances.should_receive(:normalize_dataset).
                         with(logfile_pathname, incoming_path + "core", cache_path: incoming_path + "cache", silent: true).
@@ -47,130 +40,146 @@ module Syskit::Pocolog
                         assert_equal incoming_path + "cache", s.cache_path
                         true
                     end
-
                     flexmock(datastore_m::Import).new_instances.should_receive(:move_dataset_to_store).
                         with(logfile_pathname, expected_dataset, force: false, silent: true).
                         once.pass_thru
 
-                    call_cli('auto-import', '--min-duration=0', datastore_path.to_s, logfile_pathname.dirname.to_s, silent: true)
-                    digest, time = datastore_m::Import.find_import_info(logfile_pathname)
-                    assert datastore.has?(digest)
+                    call_cli('import', '--min-duration=0', datastore_path.to_s, logfile_pathname.to_s, silent: true)
                 end
-                it "ignores datasets that have already been imported" do
-                    create_logfile('test.0.log') do
-                        create_logfile_stream 'test', metadata: Hash['rock_task_name' => 'task', 'rock_task_object_name' => 'port']
-                        write_logfile_sample Time.now, Time.now, 10
-                        write_logfile_sample Time.now + 10, Time.now + 1, 20
-                    end
-                    FileUtils.touch logfile_path('test-events.log')
-                    call_cli('auto-import', '--min-duration=0', datastore_path.to_s, logfile_pathname.dirname.to_s, silent: true)
-                    flexmock(datastore_m::Import).new_instances.should_receive(:normalize_dataset).
-                        never
-                    flexmock(datastore_m::Import).new_instances.should_receive(:move_dataset_to_store).
-                        never
-                    _out, err = capture_io do
-                        call_cli('auto-import', '--min-duration=0', datastore_path.to_s, logfile_pathname.dirname.to_s, silent: false)
-                    end
-                    assert_match /#{logfile_pathname} already seem to have been imported as .*Give --force/,
-                        err
-                end
-                it "processes datasets that have already been imported if --force is given" do
-                    create_logfile('test.0.log') do
-                        create_logfile_stream 'test', metadata: Hash['rock_task_name' => 'task', 'rock_task_object_name' => 'port']
-                        write_logfile_sample Time.now, Time.now, 10
-                        write_logfile_sample Time.now + 10, Time.now + 1, 20
-                    end
-                    FileUtils.touch logfile_path('test-events.log')
-                    call_cli('auto-import', '--min-duration=0', datastore_path.to_s, logfile_pathname.dirname.to_s, silent: true)
-                    flexmock(datastore_m::Import).new_instances.should_receive(:normalize_dataset).
-                        once.pass_thru
-                    flexmock(datastore_m::Import).new_instances.should_receive(:move_dataset_to_store).
-                        once.pass_thru
-                    _out, err = capture_io do
-                        call_cli('auto-import', '--min-duration=0', '--force', datastore_path.to_s, logfile_pathname.dirname.to_s, silent: false)
-                    end
-                    assert_match /#{logfile_pathname} seem to have already been imported but --force is given, overwriting/,
-                        err
-                end
-                it "ignores datasets that do not seem to be already imported, but are" do
-                    create_logfile('test.0.log') do
-                        create_logfile_stream 'test', metadata: Hash['rock_task_name' => 'task', 'rock_task_object_name' => 'port']
-                        write_logfile_sample Time.now, Time.now, 10
-                        write_logfile_sample Time.now + 10, Time.now + 1, 20
-                    end
-                    FileUtils.touch logfile_path('test-events.log')
-                    call_cli('auto-import', '--min-duration=0', datastore_path.to_s, logfile_pathname.dirname.to_s, silent: true)
-                    (logfile_pathname + datastore_m::Import::BASENAME_IMPORT_TAG).unlink
-                    flexmock(datastore_m::Import).new_instances.should_receive(:normalize_dataset).
-                        once.pass_thru
-                    flexmock(datastore_m::Import).new_instances.should_receive(:move_dataset_to_store).
-                        once.pass_thru
-                    _out, err = capture_io do
-                        call_cli('auto-import', '--min-duration=0', datastore_path.to_s, logfile_pathname.dirname.to_s, silent: false)
-                    end
-                    assert_match /#{logfile_pathname} already seem to have been imported as .*Give --force/,
-                        err
-                end
-                it "imports datasets that do not seem to be already imported, but are if --force is given" do
-                    create_logfile('test.0.log') do
-                        create_logfile_stream 'test', metadata: Hash['rock_task_name' => 'task', 'rock_task_object_name' => 'port']
-                        write_logfile_sample Time.now, Time.now, 10
-                        write_logfile_sample Time.now + 10, Time.now + 1, 20
-                    end
-                    FileUtils.touch logfile_path('test-events.log')
-                    call_cli('auto-import', '--min-duration=0', datastore_path.to_s, logfile_pathname.dirname.to_s, silent: true)
-                    digest, _ = datastore_m::Import.find_import_info(logfile_pathname)
-                    marker_path = datastore.core_path_of(digest) + "marker"
-                    FileUtils.touch(marker_path)
-                    (logfile_pathname + datastore_m::Import::BASENAME_IMPORT_TAG).unlink
-                    flexmock(datastore_m::Import).new_instances.should_receive(:normalize_dataset).
-                        once.pass_thru
-                    flexmock(datastore_m::Import).new_instances.should_receive(:move_dataset_to_store).
-                        once.pass_thru
-                    _out, err = capture_io do
-                        call_cli('auto-import', '--force', '--min-duration=0', datastore_path.to_s, logfile_pathname.dirname.to_s, silent: false)
-                    end
-                    assert_match /Replacing existing dataset #{digest} with new one/, err
-                    refute marker_path.exist?
-                end
-                it "ignores an empty dataset if --min-duration is non-zero" do
-                    create_logfile('test.0.log') {}
-                    FileUtils.touch logfile_path('test-events.log')
-                    incoming_path = datastore_path + 'incoming' + '0'
-                    flexmock(datastore_m::Import).new_instances.should_receive(:normalize_dataset).
-                        once.pass_thru
-                    flexmock(datastore_m::Import).new_instances.should_receive(:move_dataset_to_store).
-                        never
 
-                    call_cli('auto-import', '--min-duration=1', datastore_path.to_s, logfile_pathname.dirname.to_s, silent: true)
-                end
-                it "ignores datasets whose logical duration is lower than --min-duration" do
-                    create_logfile('test.0.log') do
-                        create_logfile_stream 'test', metadata: Hash['rock_task_name' => 'task', 'rock_task_object_name' => 'port']
-                        write_logfile_sample Time.now, Time.now, 10
-                        write_logfile_sample Time.now + 10, Time.now + 1, 20
+                describe '--auto' do
+                    it "creates the datastore path" do
+                        datastore_path.rmtree
+                        call_cli('import', '--auto', datastore_path.to_s, root_path.to_s)
+                        assert datastore_path.exist?
                     end
-                    FileUtils.touch logfile_path('test-events.log')
-                    incoming_path = datastore_path + 'incoming' + '0'
-                    flexmock(datastore_m::Import).new_instances.should_receive(:normalize_dataset).
-                        once.pass_thru
-                    flexmock(datastore_m::Import).new_instances.should_receive(:move_dataset_to_store).
-                        never
+                    it "auto-imports any directory that looks like a raw dataset" do
+                        create_logfile('test.0.log') {}
+                        FileUtils.touch logfile_path('test-events.log')
+                        incoming_path = datastore_path + 'incoming' + '0'
+                        flexmock(datastore_m::Import).new_instances.should_receive(:normalize_dataset).
+                            with(logfile_pathname, incoming_path + "core", cache_path: incoming_path + "cache", silent: true).
+                            once.pass_thru
+                        expected_dataset = lambda do |s|
+                            assert_equal incoming_path + "core", s.dataset_path
+                            assert_equal incoming_path + "cache", s.cache_path
+                            true
+                        end
 
-                    _out, err = capture_io do
-                        call_cli('auto-import', '--min-duration=5', datastore_path.to_s, logfile_pathname.dirname.to_s, silent: false)
+                        flexmock(datastore_m::Import).new_instances.should_receive(:move_dataset_to_store).
+                            with(logfile_pathname, expected_dataset, force: false, silent: true).
+                            once.pass_thru
+
+                        call_cli('import', '--auto', '--min-duration=0', datastore_path.to_s, logfile_pathname.dirname.to_s, silent: true)
+                        digest, time = datastore_m::Import.find_import_info(logfile_pathname)
+                        assert datastore.has?(digest)
                     end
-                    assert_match /#{logfile_pathname} lasts only 1.0s, ignored/, err
-                end
-            end
+                    it "ignores datasets that have already been imported" do
+                        create_logfile('test.0.log') do
+                            create_logfile_stream 'test', metadata: Hash['rock_task_name' => 'task', 'rock_task_object_name' => 'port']
+                            write_logfile_sample Time.now, Time.now, 10
+                            write_logfile_sample Time.now + 10, Time.now + 1, 20
+                        end
+                        FileUtils.touch logfile_path('test-events.log')
+                        call_cli('import', '--auto', '--min-duration=0', datastore_path.to_s, logfile_pathname.dirname.to_s, silent: true)
+                        flexmock(datastore_m::Import).new_instances.should_receive(:normalize_dataset).
+                            never
+                        flexmock(datastore_m::Import).new_instances.should_receive(:move_dataset_to_store).
+                            never
+                        _out, err = capture_io do
+                            call_cli('import', '--auto', '--min-duration=0', datastore_path.to_s, logfile_pathname.dirname.to_s, silent: false)
+                        end
+                        assert_match /#{logfile_pathname} already seem to have been imported as .*Give --force/,
+                            err
+                    end
+                    it "processes datasets that have already been imported if --force is given" do
+                        create_logfile('test.0.log') do
+                            create_logfile_stream 'test', metadata: Hash['rock_task_name' => 'task', 'rock_task_object_name' => 'port']
+                            write_logfile_sample Time.now, Time.now, 10
+                            write_logfile_sample Time.now + 10, Time.now + 1, 20
+                        end
+                        FileUtils.touch logfile_path('test-events.log')
+                        call_cli('import', '--auto', '--min-duration=0', datastore_path.to_s, logfile_pathname.dirname.to_s, silent: true)
+                        flexmock(datastore_m::Import).new_instances.should_receive(:normalize_dataset).
+                            once.pass_thru
+                        flexmock(datastore_m::Import).new_instances.should_receive(:move_dataset_to_store).
+                            once.pass_thru
+                        _out, err = capture_io do
+                            call_cli('import', '--auto', '--min-duration=0', '--force', datastore_path.to_s, logfile_pathname.dirname.to_s, silent: false)
+                        end
+                        assert_match /#{logfile_pathname} seem to have already been imported but --force is given, overwriting/,
+                            err
+                    end
+                    it "ignores datasets that do not seem to be already imported, but are" do
+                        create_logfile('test.0.log') do
+                            create_logfile_stream 'test', metadata: Hash['rock_task_name' => 'task', 'rock_task_object_name' => 'port']
+                            write_logfile_sample Time.now, Time.now, 10
+                            write_logfile_sample Time.now + 10, Time.now + 1, 20
+                        end
+                        FileUtils.touch logfile_path('test-events.log')
+                        call_cli('import', '--auto', '--min-duration=0', datastore_path.to_s, logfile_pathname.dirname.to_s, silent: true)
+                        (logfile_pathname + datastore_m::Import::BASENAME_IMPORT_TAG).unlink
+                        flexmock(datastore_m::Import).new_instances.should_receive(:normalize_dataset).
+                            once.pass_thru
+                        flexmock(datastore_m::Import).new_instances.should_receive(:move_dataset_to_store).
+                            once.pass_thru
+                        _out, err = capture_io do
+                            call_cli('import', '--auto', '--min-duration=0', datastore_path.to_s, logfile_pathname.dirname.to_s, silent: false)
+                        end
+                        assert_match /#{logfile_pathname} already seem to have been imported as .*Give --force/,
+                            err
+                    end
+                    it "imports datasets that do not seem to be already imported, but are if --force is given" do
+                        create_logfile('test.0.log') do
+                            create_logfile_stream 'test', metadata: Hash['rock_task_name' => 'task', 'rock_task_object_name' => 'port']
+                            write_logfile_sample Time.now, Time.now, 10
+                            write_logfile_sample Time.now + 10, Time.now + 1, 20
+                        end
+                        FileUtils.touch logfile_path('test-events.log')
+                        call_cli('import', '--auto', '--min-duration=0', datastore_path.to_s, logfile_pathname.dirname.to_s, silent: true)
+                        digest, _ = datastore_m::Import.find_import_info(logfile_pathname)
+                        marker_path = datastore.core_path_of(digest) + "marker"
+                        FileUtils.touch(marker_path)
+                        (logfile_pathname + datastore_m::Import::BASENAME_IMPORT_TAG).unlink
+                        flexmock(datastore_m::Import).new_instances.should_receive(:normalize_dataset).
+                            once.pass_thru
+                        flexmock(datastore_m::Import).new_instances.should_receive(:move_dataset_to_store).
+                            once.pass_thru
+                        _out, err = capture_io do
+                            call_cli('import', '--auto', '--force', '--min-duration=0', datastore_path.to_s, logfile_pathname.dirname.to_s, silent: false)
+                        end
+                        assert_match /Replacing existing dataset #{digest} with new one/, err
+                        refute marker_path.exist?
+                    end
+                    it "ignores an empty dataset if --min-duration is non-zero" do
+                        create_logfile('test.0.log') {}
+                        FileUtils.touch logfile_path('test-events.log')
+                        incoming_path = datastore_path + 'incoming' + '0'
+                        flexmock(datastore_m::Import).new_instances.should_receive(:normalize_dataset).
+                            once.pass_thru
+                        flexmock(datastore_m::Import).new_instances.should_receive(:move_dataset_to_store).
+                            never
 
-            describe "#import" do
-                it "imports a single dataset into the store" do
-                    flexmock(Syskit::Pocolog::Datastore).should_receive(:import).
-                        with(->(store) { store.datastore_path == datastore_path }, logfile_pathname, force: false, silent: true).
-                        once.pass_thru
+                        call_cli('import', '--auto', '--min-duration=1', datastore_path.to_s, logfile_pathname.dirname.to_s, silent: true)
+                    end
+                    it "ignores datasets whose logical duration is lower than --min-duration" do
+                        create_logfile('test.0.log') do
+                            create_logfile_stream 'test', metadata: Hash['rock_task_name' => 'task', 'rock_task_object_name' => 'port']
+                            write_logfile_sample Time.now, Time.now, 10
+                            write_logfile_sample Time.now + 10, Time.now + 1, 20
+                        end
+                        FileUtils.touch logfile_path('test-events.log')
+                        incoming_path = datastore_path + 'incoming' + '0'
+                        flexmock(datastore_m::Import).new_instances.should_receive(:normalize_dataset).
+                            once.pass_thru
+                        flexmock(datastore_m::Import).new_instances.should_receive(:move_dataset_to_store).
+                            never
 
-                    call_cli('import', datastore_path.to_s, logfile_pathname.to_s, silent: true)
+                        _out, err = capture_io do
+                            call_cli('import', '--auto', '--min-duration=5', datastore_path.to_s, logfile_pathname.dirname.to_s, silent: false)
+                        end
+                        assert_match /#{logfile_pathname} lasts only 1.0s, ignored/, err
+                    end
                 end
             end
 
