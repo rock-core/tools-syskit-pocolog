1124109dadc7c9a3e2d2188477923902b56a57bc
Sylvain <sylvain.joyeux@tidewise.io>
2020-11-16 09:21:38 -0300

    daru: don't assume the estimated number of rows is equal to the actual
    It depends on the initialization (where we wait on every column having
    a sample)

--- lib/syskit/log/daru/create_aligned_frame.rb
+++ lib/syskit/log/daru/create_aligned_frame.rb
@@ -9,7 +9,7 @@ module Syskit
 
                 vectors = builders.map { |b| b.create_vectors(size) }
 
-                row = 0
+                row_count = 0
                 joint_stream.raw_each do |index, time, sample|
                     current_row[index] = [time, sample]
 
@@ -20,14 +20,17 @@ module Syskit
 
                     current_row.each_with_index do |(v_time, v_sample), v_index|
                         builders[v_index].update_row(
-                            vectors[v_index], row, v_time, v_sample
+                            vectors[v_index], row_count, v_time, v_sample
                         )
                     end
-                    row += 1
+                    row_count += 1
                 end
 
-                builders.each_with_index do |b, i|
-                    b.recenter_time_vectors(vectors[i], center_time)
+                # Resize the vectors
+                vectors = builders.zip(vectors).map do |b, v|
+                    v = b.truncate_vectors(v, row_count)
+                    b.recenter_time_vectors(v, center_time)
+                    v
                 end
 
                 vectors.flatten
