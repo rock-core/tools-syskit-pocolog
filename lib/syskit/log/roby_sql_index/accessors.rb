3a2c60d1901948b3a3a6121d04705f3d1fec5abf
Sylvain <sylvain.joyeux@tidewise.io>
2020-11-21 12:20:28 -0300

    log: add task and event instance access

--- lib/syskit/log/roby_sql_index/accessors.rb
+++ lib/syskit/log/roby_sql_index/accessors.rb
@@ -74,11 +74,16 @@ module Syskit
                 # It can give access to the constants under it, or to the events
                 # that are known to the index
                 class TaskModel < Namespace
+                    # The task model name
+                    attr_reader :name
+                    # A unique ID for this task model
+                    attr_reader :id
+
                     def initialize(index, name)
                         super(index, name)
                         @name = name
-                        model = @index.models.where(name: name).one!
-                        @query = @index.tasks.where(model_id: model.id)
+                        @id = @index.models.where(name: name).one!.id
+                        @query = @index.tasks.where(model_id: id)
                     end
 
                     def each_event
@@ -87,15 +92,38 @@ module Syskit
                         @index.history_of(@query)
                               .select(:name).distinct
                               .pluck(:name).each do |event_name|
-                                  yield(EventModel.new(@index, event_name, @query))
+                                  yield(EventModel.new(@index, event_name, self))
                               end
+                    end
+
+                    # Return the event model with the given name
+                    #
+                    # @raise ArgumentError if there are no events with that name
+                    def event(name)
+                        unless @index.history_of(@query).where(name: name).first
+                            raise ArgumentError, "no events named '#{name}' in #{self}'"
+                        end
+
+                        EventModel.new(@index, name, self)
+                    end
+
+                    def each_task
+                        return enum_for(__method__) unless block_given?
+
+                        @query.each do |obj|
+                            yield Task.new(@index, obj.id, self)
+                        end
+                    end
+
+                    def ==(other)
+                        other.kind_of?(TaskModel) && other.id == id
                     end
 
                     def method_missing(m, *args, **kw, &block)
                         m_to_s = m.to_s
                         return super unless m_to_s.end_with?("_event")
 
-                        event_name = m_to_s.gsub(/_event$/, "")
+                        event_name = m_to_s[0..-7]
                         unless @index.event_with_name?(event_name)
                             raise NoMethodError.new(m),
                                   "no events named #{event_name} have been emitted"
@@ -107,34 +135,173 @@ module Syskit
                                   .exist?
 
                         unless has_events
-                            raise NoMethodError.new(m),
+                            raise NoMethodError.new("", m),
                                   "there are emitted events named #{event_name}, but "\
                                   "not for a task of model #{@name}"
                         end
 
-                        EventModel.new(@index, event_name, @query)
+                        EventModel.new(@index, event_name, self)
+                    end
+
+                    # Return the task instance object with the given ID
+                    def by_id(id)
+                        Task.new(@index, id, self)
+                    end
+
+                    # @api private
+                    #
+                    # The query that returns the task IDs of the instances of this model
+                    def task_ids
+                        @query.pluck(:id)
                     end
                 end
 
                 # Represents an event generator model
                 class EventModel
+                    # The event's name
                     attr_reader :name
+                    # This event's task model
+                    attr_reader :task_model
 
-                    def initialize(index, name, task_query)
+                    def initialize(index, name, task_model)
                         @index = index
                         @name = name
+                        @task_model = task_model
                         @query = @index.emitted_events
-                                       .where(name: name, task_id: task_query.pluck(:id))
+                                       .where(name: name, task_id: task_model.task_ids)
+                    end
+
+                    def ==(other)
+                        other.kind_of?(EventModel) &&
+                            other.name == name &&
+                            other.task_model == task_model
                     end
 
                     # List the matching event emissions
-                    def each_emission(&block)
-                        @query.combine(task: :model).each(&block)
+                    def each_emission
+                        return enum_for(__method__) unless block_given?
+
+                        @query.each do |obj|
+                            yield Event.new(@index, obj.id, obj.time, obj.name,
+                                            @task_model.by_id(obj.task_id), self)
+                        end
                     end
 
                     # Get the first emission
                     def first_emission
                         each_emission.first
+                    end
+                end
+
+                # Represents a task instance
+                class Task
+                    # A unique ID
+                    attr_reader :id
+                    # The task model
+                    #
+                    # @return [TaskModel]
+                    attr_reader :model
+
+                    def initialize(index, id, model)
+                        @index = index
+                        @id = id
+                        @model = model
+                    end
+
+                    # Return the task's activation interval
+                    #
+                    # This is named like this to match Pocolog::DataStream's interface
+                    def interval_lg
+                        [start_event.first.time, stop_event.first.time]
+                    end
+
+                    def ==(other)
+                        other.kind_of?(Task) && other.id == id
+                    end
+
+                    def each_emission(**where)
+                        return enum_for(__method__, **where) unless block_given?
+
+                        query = @index.emitted_events
+                                      .where(task_id: @id, **where)
+                        query.each do |emission|
+                            name = emission.name
+                            yield Event.new(@index, emission.id, emission.time, name,
+                                            self, model.event(name))
+                        end
+                    end
+
+                    def event(name)
+                        BoundEvent.new(@index, name, self, model.event(name))
+                    end
+
+                    def respond_to_missing?(m, include_private = false)
+                        super || m.to_s.end_with?("_event")
+                    end
+
+                    def method_missing(m, *args, **kw, &block)
+                        if m.to_s.end_with?("_event")
+                            unless args.empty? && kw.empty?
+                                raise ArgumentError, "wrong number of arguments"
+                            end
+                            event(m[0..-7])
+                        else
+                            super
+                        end
+                    end
+                end
+
+                # An event model bound to a particular task instance
+                class BoundEvent
+                    # The event name
+                    attr_reader :name
+                    # The task it is bound to
+                    attr_reader :task
+                    # The event model
+                    attr_reader :model
+
+                    def initialize(index, name, task, model)
+                        @index = index
+                        @name = name
+                        @task = task
+                        @model = model
+                    end
+
+                    def each_emission(&block)
+                        task.each_emission(name: name, &block)
+                    end
+
+                    def first
+                        each_emission.first
+                    end
+                end
+
+                # Represents an emitted event
+                class Event
+                    # The event's emission time
+                    attr_reader :time
+                    # The event's name
+                    attr_reader :name
+
+                    # The event's task
+                    attr_reader :task
+                    # The event's model
+                    attr_reader :model
+
+                    # A unique ID for this event
+                    attr_reader :id
+
+                    def ==(other)
+                        other.kind_of?(Event) && other.id == id
+                    end
+
+                    def initialize(index, id, time, name, task, model) # rubocop:disable Metrics/ParameterLists
+                        @index = index
+                        @id = id
+                        @time = time
+                        @name = name
+                        @task = task
+                        @model = model
                     end
                 end
             end
--- test/roby_sql_index/accessors_test.rb
+++ test/roby_sql_index/accessors_test.rb
@@ -65,13 +65,75 @@ module Syskit
                         assert_equal :not_an_event, e.name, e.message
                     end
 
+                    it "gives access to task instances" do
+                        tasks = @task_model.each_task.to_a
+                        assert_equal 1, tasks.size
+
+                        t = tasks.first
+                        assert_equal @task_model, t.model
+                    end
+
                     it "gives access to an existing event" do
-                        assert @task_model.start_event
+                        ev = @task_model.start_event
+                        assert_equal "start", ev.name
+                        assert_equal @task_model, ev.task_model
                     end
 
                     it "allows to enumerate all events of the task model" do
                         assert_equal %w[start failed stop],
                                      @task_model.each_event.map(&:name)
+                    end
+                end
+
+                describe "an event model" do
+                    before do
+                        @event_model = @root.Namespace.M.start_event
+                    end
+
+                    it "allows to enumerate its emissions" do
+                        emissions = @event_model.each_emission.to_a
+                        assert_equal 1, emissions.size
+
+                        ev = emissions.first
+                        assert_equal "start", ev.name
+                        assert_equal @event_model, ev.model
+                        assert_equal ev.model.task_model.each_task.first,
+                                     ev.task
+                    end
+                end
+
+                describe "a task" do
+                    before do
+                        @task = @root.Namespace.M.each_task.first
+                    end
+
+                    it "allows to enumerate its emissions" do
+                        emissions = @task.each_emission.to_a
+                        assert_equal 3, emissions.size
+                        assert_equal %w[start failed stop], emissions.map(&:name)
+                        assert_equal [@task, @task, @task], emissions.map(&:task)
+                    end
+
+                    it "gives access to a specific bound event" do
+                        emissions = []
+                        @task.start_event.each_emission do |e|
+                            emissions << e
+                        end
+
+                        assert_equal 1, emissions.size
+
+                        ev = emissions.first
+                        assert_equal "start", ev.name
+                        assert_equal @task, ev.task
+                    end
+
+                    it "gives access to a specific bound event" do
+                        emissions = @task.start_event.each_emission.to_a
+                        assert_equal 1, emissions.size
+
+                        ev = emissions.first
+                        assert_equal "start", ev.name
+                        assert_equal @task, ev.task
                     end
                 end
 
@@ -83,7 +145,12 @@ module Syskit
                     it "allows to enumerate the event's emissions" do
                         emissions = @event_model.each_emission.to_a
                         assert_equal 1, emissions.size
-                        assert_kind_of Entities::EmittedEvent, emissions.first
+
+                        ev = emissions.first
+                        assert_equal "start", ev.name
+                        assert_equal @event_model, ev.model
+                        assert_equal ev.model.task_model.each_task.first,
+                                     ev.task
                     end
                 end
             end
